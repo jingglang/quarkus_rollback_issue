f4bff9fe6d950f4164f1807b249d619c115ff2bc
Amrullah <amrus@cssimpact.com>
2023-04-19 22:59:41 +0700

    specify transaction manager to AgroalDataSource

--- src/main/java/org/acme/test/MyTenantConnectionResolver.java
+++ src/main/java/org/acme/test/MyTenantConnectionResolver.java
@@ -1,7 +1,14 @@
 package org.acme.test;
 
 import io.agroal.api.AgroalDataSource;
+import io.agroal.api.configuration.supplier.AgroalConnectionFactoryConfigurationSupplier;
+import io.agroal.api.configuration.supplier.AgroalConnectionPoolConfigurationSupplier;
+import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
 import io.agroal.api.configuration.supplier.AgroalPropertiesReader;
+import io.agroal.api.security.NamePrincipal;
+import io.agroal.api.security.SimplePassword;
+import io.agroal.api.transaction.TransactionIntegration;
+import io.agroal.narayana.NarayanaTransactionIntegration;
 import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.customized.QuarkusConnectionProvider;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantConnectionResolver;
@@ -10,54 +17,65 @@ import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.TransactionManager;
+import javax.transaction.TransactionSynchronizationRegistry;
 import java.sql.SQLException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
  * @author saka
  */
 @ApplicationScoped
 @PersistenceUnitExtension
 public class MyTenantConnectionResolver implements TenantConnectionResolver {
 	private Logger log = Logger.getLogger(MyTenantConnectionResolver.class);
-	@ConfigProperty(name = "quarkus.datasource.tenant.username")
+	@ConfigProperty(name = "quarkus.datasource.username")
 	String username;
-	@ConfigProperty(name = "quarkus.datasource.tenant.password")
+	@ConfigProperty(name = "quarkus.datasource.password")
 	String password;
+	@Inject
+	TransactionManager transactionManager;
+	@Inject
+	TransactionSynchronizationRegistry transactionSynchronizationRegistry;
+
 	@Override
 	public ConnectionProvider resolve(String tenantId) {
 		return new QuarkusConnectionProvider(createDatasource(tenantId));
 	}
 
 	private AgroalDataSource createDatasource(String tenantId) {
-		
 
 		try {
-			Map<String, String> props = new HashMap<>();
 
-			props.put(AgroalPropertiesReader.MAX_SIZE, "10");
-			props.put(AgroalPropertiesReader.MIN_SIZE, "2");
-			props.put(AgroalPropertiesReader.INITIAL_SIZE, "2");
-			props.put(AgroalPropertiesReader.MAX_LIFETIME_S, "57");
-			props.put(AgroalPropertiesReader.ACQUISITION_TIMEOUT_S, "54");
-			props.put(AgroalPropertiesReader.PROVIDER_CLASS_NAME, "org.mariadb.jdbc.Driver");
-			props.put(AgroalPropertiesReader.JDBC_URL, "jdbc:mariadb://localhost:3306/country_" + tenantId);
-			props.put(AgroalPropertiesReader.PRINCIPAL, username);
-			props.put(AgroalPropertiesReader.CREDENTIAL, password);
+			AgroalDataSourceConfigurationSupplier dataSourceConfiguration = new AgroalDataSourceConfigurationSupplier();
 
-			AgroalDataSource datasource = AgroalDataSource.from(new AgroalPropertiesReader()
-					  .readProperties(props)
-					  .get());
+			AgroalConnectionPoolConfigurationSupplier poolConfiguration = dataSourceConfiguration.connectionPoolConfiguration();
 
-//			long activeCount = datasource.getMetrics().activeCount();
-//			long availableCount = datasource.getMetrics().availableCount();
-			log.infof("datasource for tenant %s objectId %s healthy status %s", tenantId,System.identityHashCode(datasource), datasource.isHealthy(false));
+			TransactionIntegration txIntegration = new NarayanaTransactionIntegration(transactionManager, transactionSynchronizationRegistry, null, false, null);
+			poolConfiguration
+					  .initialSize(2)
+					  .maxSize(10)
+					  .minSize(2)
+					  .maxLifetime(Duration.of(5, ChronoUnit.MINUTES))
+					  .acquisitionTimeout(Duration.of(30, ChronoUnit.SECONDS))
+					  .transactionIntegration(txIntegration);
+
+			AgroalConnectionFactoryConfigurationSupplier connectionFactoryConfiguration = poolConfiguration.connectionFactoryConfiguration();
+
+			connectionFactoryConfiguration
+					  .jdbcUrl("jdbc:mariadb://localhost:3306/country_" + tenantId)
+					  .credential(new NamePrincipal(username))
+					  .credential(new SimplePassword(password));
+
+
+			AgroalDataSource datasource = AgroalDataSource.from(dataSourceConfiguration.get());
 			return datasource;
 		} catch (SQLException ex) {
-			throw new IllegalStateException("Failed to create a new data source based on the existing datasource configuration",
-					  ex);
+			throw new IllegalStateException("Failed to create a new data source based on the existing datasource configuration", ex);
 		}
 	}
 
